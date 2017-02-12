197619f84266097c88efac33f251d40b82f6fc96
Kenneth I Nielsen <battle_stalker@hotmail.com>
2017-02-12 16:12:30 +0100

    Minor changes

--- MicroservicesPlatform/API/src/main/java/domain/APICustomerController.java
+++ MicroservicesPlatform/API/src/main/java/domain/APICustomerController.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
